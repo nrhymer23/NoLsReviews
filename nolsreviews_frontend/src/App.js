146e679c16ec40fc6e5d66ed3a0418e0ce193f6a
nrhymer23 <38295535+nrhymer23@users.noreply.github.com>
2022-07-28 06:59:39 -0400

    frontend setup complete

--- nolsreviews_frontend/src/App.js
+++ nolsreviews_frontend/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Route, Routes, useNavigate } from 'react-router-dom'
+import Home from './components/Home'
+import Login from './components/Login'
+
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-        Hello World
-    </h1>
+    <Routes>
+      <Route path="login" element={<Login />} />
+      <Route path="/*" element={<Home />} />
+    </Routes>
   )
 }
 
--- nolsreviews_frontend/src/index.js
+++ nolsreviews_frontend/src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom'; // Needed to make app multipage
 
 import App from './App';
 import './index.css';
 
-ReactDom.render(<App />, document.getElementById('root'));
+ReactDom.render(
+    <Router> 
+        <App />
+    </Router>,
+    document.getElementById('root')
+    );
